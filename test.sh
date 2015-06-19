5cbc7d136d2fa94c0b12d8384b73d4fe9be3557b
xusiwei1236 <xusiwei1236@163.com>
2015-06-19 16:19:33 +0800

    pretty result file name

--- test.sh
+++ test.sh
@@ -5,21 +5,21 @@ make
 
 #tests:
 echo "benchmark I: Oxford Dictionary word counting."
-./b1 data/oxfordDict.txt
+./b1 data/oxfordDict.txt | tee result1.txt
 
 echo "benchmark II: random int counting."
 for ((i = 1024; i < 1000*1000; i *= 2))
 do
-	./b2 $i | tee -a res2.txt
+	./b2 $i | tee -a result2.txt
 done
 
 for ((i = 10000; i < 100*10000; i += 10000))
 do
-	./b2 $i | tee -a res22.txt
+	./b2 $i | tee -a result22.txt
 done
 
 echo "benchmark III: random insert and random find."
 for ((i = 10000; i < 100*10000; i += 10000))
 do
-	./b3 $i | tee -a res33.txt
+	./b3 $i | tee -a result3.txt
 done
